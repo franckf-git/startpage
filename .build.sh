5987810a4e1573acf4c9367f97b7971dbf6c5a22
franckf <efydd.artio@tuta.io>
2020-12-12 17:52:16 +0100

    [FEAT] (build): /index create markdown - to be convert in deploy

--- .build.sh
+++ .build.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-sed -i "s/teststring/remplacement/g" index.html
+echo "$(cat README.md |
+sed -r 's/^## (.*)/<h2>\1<\/h2>/g' |                #entete2
+sed -r 's/\[(.*)\]\((.*)\)/<a href="\2">\1<\/a>/g' |         #url
+sed -r 's/^\* (.*)/<ul>\n<li>\1<\/li>\n<\/ul>/g' |               #liste
+sed -r 's/(.*>)<\/br>$/\1/g'                               #retour a ligne
+)" > index.html
