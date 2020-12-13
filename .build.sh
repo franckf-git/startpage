8262db20ccda44bd2b9f04588e9945b96fb0646c
franckf <efydd.artio@tuta.io>
2020-12-13 15:43:47 +0100

    [STYLE] (h2): remove style (too much space)

--- .build.sh
+++ .build.sh
@@ -3,7 +3,7 @@
 echo "$(cat README.md |
     sed -r 's/^STARTCOLUMN$/<div class="column">/g' |
     sed -r 's/^ENDCOLUMN$/<\/div>/g' |
-    sed -r 's/^## (.*)/<h2 class="subtitle has-text-light">\1<\/h2>/g' |
+    sed -r 's/^## (.*)/<h2 class="has-text-light">\1<\/h2>/g' |
     sed -r 's/\[(.*)\]\((.*)\)/<a href="\2">\1<\/a>/g' |
     sed -r 's/^\* (.*)/<ul>\n<li>\1<\/li>\n<\/ul>/g' |
     sed -r 's/(.*>)<\/br>$/\1/g'
