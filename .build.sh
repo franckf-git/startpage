5670661b23b1ef1c0bc0ee0a9afbcf50d82d645f
franckf <efydd.artio@tuta.io>
2020-12-13 15:12:36 +0100

    [STYLE] (h2): white text

--- .build.sh
+++ .build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "$(cat README.md |
-    sed -r 's/^## (.*)/<h2>\1<\/h2>/g' |
+    sed -r 's/^## (.*)/<h2 class="has-text-light">\1<\/h2>/g' |
     sed -r 's/\[(.*)\]\((.*)\)/<a href="\2">\1<\/a>/g' |
     sed -r 's/^\* (.*)/<ul>\n<li>\1<\/li>\n<\/ul>/g' |
     sed -r 's/(.*>)<\/br>$/\1/g'
