140a28b0ae3c14d248544ca211ae6da20e9195b1
franckf <efydd.artio@tuta.io>
2020-12-27 13:23:41 +0100

    feat: build with html separated

--- .build.sh
+++ .build.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 
-echo "$(cat README.md |
+echo " \
+    $(cat .header.html) \
+    $(cat README.md |
     sed -r 's/^STARTCOLUMN$/<div class="column">/g' |
     sed -r 's/^ENDCOLUMN$/<\/div>/g' |
     sed -r 's/^## (.*)/<h2 class="has-text-light">\1<\/h2>/g' |
     sed -r 's/\[(.*)\]\((.*)\)/<a href="\2">\1<\/a>/g' |
     sed -r 's/^\* (.*)/<ul>\n<li>\1<\/li>\n<\/ul>/g' |
     sed -r 's/(.*>)<\/br>$/\1/g'
-    )" > index.html
+    ) \
+    $(cat .footer.html) \
+    " > index.html
 
