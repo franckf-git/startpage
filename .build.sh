45a15a55c4d74a6565ba772d825c44a5baa5dd7f
franckf <efydd.artio@tuta.io>
2021-01-04 18:15:12 +0100

    feat: (structure) open links in new tabs

--- .build.sh
+++ .build.sh
@@ -6,7 +6,7 @@ echo " \
     sed -r 's/^STARTCOLUMN$/<div class="column">/g' |
     sed -r 's/^ENDCOLUMN$/<\/div>/g' |
     sed -r 's/^## (.*)/<h2 class="has-text-light">\1<\/h2>/g' |
-    sed -r 's/\[(.*)\]\((.*)\)/<a href="\2">\1<\/a>/g' |
+    sed -r 's/\[(.*)\]\((.*)\)/<a href="\2" target="_blank">\1<\/a>/g' |
     sed -r 's/^\* (.*)/<ul>\n<li>\1<\/li>\n<\/ul>/g' |
     sed -r 's/(.*>)<\/br>$/\1/g'
     ) \
