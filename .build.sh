8635994104f3bc28a38f17580398012b4841539d
franckf <efydd.artio@tuta.io>
2021-01-30 08:40:26 +0100

    style: fullwidth buttons

--- .build.sh
+++ .build.sh
@@ -8,7 +8,7 @@ echo " \
     sed -r 's/^## (.*)/<h2 class="has-text-light">\1<\/h2>/g' |
     sed -r 's/\[(.*)\]\((.*)\)/<a href="\2" target="_blank">\1<\/a>/g' |
     sed -r 's/^\* (.*)/<ul>\n<li>\1<\/li>\n<\/ul>/g' |
-    sed -r 's/^- <a (.*)/<a class="button" \1/g' |
+    sed -r 's/^- <a (.*)/<a class="button is-fullwidth is-outlined" \1/g' |
     sed -r 's/(.*>)<\/br>$/\1/g'
     ) \
     $(cat .footer.html) \
